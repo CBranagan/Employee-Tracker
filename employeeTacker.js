6f41057c3193572812cdf040e855ae35575b4d13
Chad Branagan <chad.branagan@gmail.com>
2022-02-24 17:41:17 -0800

    sql connection and start db

---
+++ employeeTacker.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Sara8755",
+  database: "employee_db",
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(err);
+  // mainMenu();
+});
